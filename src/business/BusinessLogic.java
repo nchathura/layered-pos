0674c0e05ecf0702a78c13f7e5f176e21b67479d
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2020-07-27 17:46:10 +0530

    Stage 1: Finish business layer

--- src/business/BusinessLogic.java
+++ src/business/BusinessLogic.java
@@ -1,4 +1,53 @@
 package business;
 
+import dao.DataLayer;
+import db.DBConnection;
+import util.CustomerTM;
+import util.ItemTM;
+import util.OrderDetailTM;
+import util.OrderTM;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BusinessLogic {
+
+    public static List<CustomerTM> getAllCustomers(){
+        return DataLayer.getAllCustomers();
+    }
+
+    public static boolean saveCustomer(CustomerTM customer){
+        return DataLayer.saveCustomer(customer);
+    }
+
+    public static boolean deleteCustomer(String customerId){
+        return DataLayer.deleteCustomer(customerId);
+    }
+
+    public static boolean updateCustomer(CustomerTM customer){
+        return DataLayer.updateCustomer(customer);
+    }
+
+    public static List<ItemTM> getAllItems(){
+        return DataLayer.getAllItems();
+    }
+
+    public static boolean saveItem(ItemTM item){
+        return DataLayer.saveItem(item);
+    }
+
+    public static boolean deleteItem(String itemCode){
+        return DataLayer.deleteItem(itemCode);
+    }
+
+    public static boolean updateItem(ItemTM item){
+        return DataLayer.updateItem(item);
+    }
+
+    public static boolean placeOrder(OrderTM order, List<OrderDetailTM> orderDetails){
+        return DataLayer.placeOrder(order, orderDetails);
+    }
+
+
 }
